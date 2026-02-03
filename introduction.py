7a38fb8a8936dd3cb9bbee5bfaf0dc21f9e322e1
leshoj-code <leshyoj@gmail.com>
2026-02-03 13:20:47 +0300

    Python

---
+++ introduction.py
@@ -0,0 +1,6 @@
+print("Welcome to Python Programming")
+print("Leshan")
+print(200)
+print(40*3)
+
+#Print is a method used to display an output
---
+++ variables.py
@@ -0,0 +1,17 @@
+# Variable is a container used to store a value
+
+school = "eMobilis"
+firstname = "Leshan"
+lastname = "Smith"
+num1 = 45
+num2 = 100
+language = "Python"
+
+print(school)
+print(firstname)
+print(lastname)
+print(firstname, lastname)
+print(school, "is a coding school")
+print(language, "is a web programming language")
+
+print("The sum of", num1, "and", num2, "is", num1+num2)
